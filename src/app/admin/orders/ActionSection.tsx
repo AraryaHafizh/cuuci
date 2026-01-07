7b45714c552750db10f341bd3085c9e95303b4b9
AraryaHafizh <araryahafizh@gmail.com>
2026-01-07 23:18:40 +0700

    feat: connect admin tables

--- src/app/admin/orders/ActionSection.tsx
+++ src/app/admin/orders/ActionSection.tsx
@@ -11,19 +11,38 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Dispatch, SetStateAction } from "react";
 
-export function ActionSection() {
+export function ActionSection({
+  setSearch,
+  setStatus,
+}: {
+  setSearch: Dispatch<SetStateAction<string>>;
+  setStatus: Dispatch<SetStateAction<string>>;
+}) {
   return (
     <section className="mt-10 mb-5 flex gap-2">
-      <Input placeholder="Search by order id" />
-      <SortDropdown />
+      <Input
+        placeholder="Search by order id"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <SortDropdown setStatus={setStatus} />
     </section>
   );
 }
 
-function SortDropdown() {
+function SortDropdown({
+  setStatus,
+}: {
+  setStatus: Dispatch<SetStateAction<string>>;
+}) {
+  const handleChange = (value: string) => {
+    setStatus(value === "ALL" ? "" : value);
+    console.log(value);
+  };
+
   return (
-    <Select>
+    <Select onValueChange={handleChange}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Filter by status" />
       </SelectTrigger>
@@ -31,7 +50,7 @@ function SortDropdown() {
         <SelectGroup>
           <SelectLabel>Roles</SelectLabel>
           {Object.entries(filterStatus).map(([key, value], i) => (
-            <SelectItem key={i} value={value}>
+            <SelectItem key={i} value={key}>
               {value}
             </SelectItem>
           ))}
